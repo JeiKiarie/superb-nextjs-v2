122c4e8cc52d2b47a1f0a49abf1b677036199ffe
Joseph Kiarie <microstation.k@gmail.com>
2024-06-29 01:50:42 +0300

    added faqs section

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,6 +3,16 @@ import img1 from '../../public/images/AboutPage/SeniorCitizenExerciseClass.webp'
 import aboutHeroImg from '../../public/images/pexels-kampus-7551667.jpg';
 import Image from 'next/image';
 import Link from 'next/link';
+import {
+	Accordion,
+	AccordionActions,
+	AccordionDetails,
+	AccordionSummary,
+	Typography,
+} from '@mui/material';
+import { MdOutlineExpandMore } from 'react-icons/md';
+import { ExpandMore } from '@mui/icons-material';
+// import { MdExpandMore } from 'react-icons/md';
 
 const About = () => {
 	return (
@@ -37,6 +47,78 @@ const About = () => {
 					<Button>
 						<Link href="/contact">Contact Us</Link>
 					</Button>
+				</div>
+			</section>
+			<section className="faqs">
+				<div className="section-title">
+					<h3>frequently asked questions</h3>
+				</div>
+				<div className="faqs-container">
+					<Accordion
+						className="accordion"
+						slotProps={{ transition: { unmountOnExit: true } }}>
+						<AccordionSummary
+							expandIcon={<ExpandMore sx={{ fontSize: '3rem' }} />}
+							sx={{}}
+							className="accordion-summary">
+							<Typography variant="h4">Do you offer live-in care?</Typography>
+						</AccordionSummary>
+						<AccordionDetails sx={{}}>
+							<Typography
+								variant="p"
+								className="accordion-details">
+								Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+								Voluptates impedit tenetur repellat. Maxime voluptatibus eius
+								iure architecto debitis placeat voluptatum error, vitae saepe,
+								quasi totam quidem, dignissimos eaque nobis vel!
+							</Typography>
+						</AccordionDetails>
+						{/* <AccordionActions></AccordionActions> */}
+					</Accordion>
+					<Accordion
+						className="accordion"
+						slotProps={{ transition: { unmountOnExit: true } }}>
+						<AccordionSummary
+							expandIcon={<ExpandMore sx={{ fontSize: '3rem' }} />}
+							sx={{}}
+							className="accordion-summary">
+							<Typography variant="h4">
+								How qualified are your carers?
+							</Typography>
+						</AccordionSummary>
+						<AccordionDetails sx={{}}>
+							<Typography
+								variant="p"
+								className="accordion-details">
+								Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+								Voluptates impedit tenetur repellat. Maxime voluptatibus eius
+								iure architecto debitis placeat voluptatum error, vitae saepe,
+								quasi totam quidem, dignissimos eaque nobis vel!
+							</Typography>
+						</AccordionDetails>
+						{/* <AccordionActions></AccordionActions> */}
+					</Accordion>
+					<Accordion
+						className="accordion"
+						slotProps={{ transition: { unmountOnExit: true } }}>
+						<AccordionSummary
+							expandIcon={<ExpandMore sx={{ fontSize: '3rem' }} />}
+							sx={{}}
+							className="accordion-summary">
+							<Typography variant="h4">Do I pay the carer directly?</Typography>
+						</AccordionSummary>
+						<AccordionDetails sx={{}}>
+							<Typography
+								variant="p"
+								className="accordion-details">
+								Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+								Voluptates impedit tenetur repellat. Maxime voluptatibus eius
+								iure architecto debitis placeat voluptatum error, vitae saepe,
+								quasi totam quidem, dignissimos eaque nobis vel!
+							</Typography>
+						</AccordionDetails>
+						{/* <AccordionActions></AccordionActions> */}
+					</Accordion>
 				</div>
 			</section>
 		</main>
