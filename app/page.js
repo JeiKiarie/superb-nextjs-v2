d1f3b1c40f9e707c5e66f5604321b8f7572094bd
Joseph Kiarie <microstation.k@gmail.com>
2024-07-07 15:12:37 +0300

    additions to home page content

--- app/page.js
+++ app/page.js
@@ -9,6 +9,12 @@ import { BiBrain } from 'react-icons/bi';
 import { LuCalendarClock } from 'react-icons/lu';
 import { TbMoodPuzzled } from 'react-icons/tb';
 import { GiPeaceDove } from 'react-icons/gi';
+import { MdHealthAndSafety } from 'react-icons/md';
+import { GiBroom } from 'react-icons/gi';
+import { FaPeopleGroup } from 'react-icons/fa6';
+import { BiFirstAid } from 'react-icons/bi';
+import { BiTimeFive } from 'react-icons/bi';
+import { BiAlarm } from 'react-icons/bi';
 // import { HiChevronDoubleDown } from 'react-icons/hi';
 import { FaArrowDownLong } from 'react-icons/fa6';
 import { Box, Flex, Text, Divider, Button, Icon } from '@chakra-ui/react';
@@ -20,6 +26,43 @@ import Link from 'next/link';
 const services = [
 	{
 		id: 1,
+		title: 'Personal Care',
+		icon: MdHealthAndSafety,
+		text: `Our personal care services are designed to assist with daily activities, ensuring that clients can maintain their independence and dignity.`,
+	},
+	{
+		id: 2,
+		title: 'Household Assistance',
+		icon: GiBroom,
+		text: `We take care of household chores to create a clean, safe, and comfortable living environment.`,
+	},
+	{
+		id: 3,
+		title: 'Companionship',
+		icon: FaPeopleGroup,
+		text: `We provide companionship to combat loneliness and enhance quality of life.`,
+	},
+	{
+		id: 4,
+		title: 'Specialized Care',
+		icon: BiFirstAid,
+		text: `We offer specialized care for those with specific health conditions or recovery needs.`,
+	},
+
+	{
+		id: 5,
+		title: '24/7 Care & Support',
+		icon: BiTimeFive,
+		text: `Our dedicated caregivers provide round-the-clock assistance, ensuring safety and comfort at all times. Whether it’s help with daily activities, medical needs, or companionship, we are here to support you every step of the way.`,
+	},
+	{
+		id: 6,
+		title: 'Emergency Response',
+		icon: BiAlarm,
+		text: `Trained in emergency procedures, our caregivers can quickly and effectively respond to all unexpected situations.`,
+	},
+	{
+		id: 7,
 		title: 'Skilled Nursing Care',
 		icon: LiaUserNurseSolid,
 		text: `Our skilled nursing services are delivered by a
@@ -27,25 +70,25 @@ const services = [
 		meeting the complex medical needs of our residents.`,
 	},
 	{
-		id: 2,
+		id: 8,
 		title: 'Assisted Living',
 		icon: MdOutlineAssistWalker,
 		text: `Our assisted living communities are carefully designed to promote independence and well-being while offering personalized assistance with daily tasks.`,
 	},
 	{
-		id: 3,
+		id: 9,
 		title: 'Memory Care',
 		icon: TbMoodPuzzled,
 		text: `Our memory care program provides a safe and secure environment where residents receive specialized care tailored to their cognitive needs.`,
 	},
 	{
-		id: 4,
+		id: 10,
 		title: 'Respite Care',
 		icon: LuCalendarClock,
 		text: `Our respite care services offer temporary relief for caregivers while ensuring that their loved ones receive the highest standard of care in a welcoming and supportive setting.`,
 	},
 	{
-		id: 5,
+		id: 11,
 		title: 'Hospice Care',
 		icon: GiPeaceDove,
 		text: `Our holistic approach focuses on pain management, symptom control, and emotional support, allowing residents to live their remaining days with dignity and peace.`,
@@ -127,10 +170,10 @@ const Home = () => {
 						marginInline={'auto'}
 						textAlign={'center'}>
 						Elderly care and supported living services. With our team of
-						experienced and compassionate caregivers, we offer personalized, customised
-						live-in care services tailored to meet all unique needs, ensuring
-						that you receive the support you need in the most comfortable and
-						effective way possible.
+						experienced and compassionate caregivers, we offer personalized,
+						customised live-in care services tailored to meet all unique needs,
+						ensuring that you receive the support you need in the most
+						comfortable and effective way possible.
 					</Text>
 				</Box>
 
@@ -154,7 +197,13 @@ const Home = () => {
 								<Box
 									padding={'0.4rem'}
 									color={'#cca6c8'}>
-									{<service.icon fontSize={'6rem'} />}
+									{
+										<service.icon
+											fontSize={'6rem'}
+											width={'6rem'}
+											height={'6rem'}
+										/>
+									}
 								</Box>
 
 								<Text as={'h3'}>{service.title}</Text>
